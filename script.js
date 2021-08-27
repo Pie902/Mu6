cda18308773d4a3fdbb386769528953e2869f6ee
Pie902 <87331370+Pie902@users.noreply.github.com>
2021-08-27 10:11:44 -0600

    Added Menu

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 console.log("Hey there!");
 
 var open = false;
-var value = 100
-var xpos = document.getElementById("menu").style.margin-left
+var value = 100;
+var pos = -320;
+var ypos = document.getElementById("menu");
+var menuOpen = false;
 
 function menu() {
     if (open == false) {
@@ -28,3 +30,30 @@ function menu() {
 //}
 
 //setInterval(open(), 5)
+
+function updatemenu() {
+    if (menuOpen == true) {
+      menuOpen = false
+      document.getElementById('menu').style.borderBottomRightRadius = '0';
+      document.getElementById('menu').style.borderBottomLeftRadius = '0';
+    }else{
+      menuOpen = true
+      document.getElementById('menu').style.borderRadius = '10px';
+    }
+}
+
+function animatemenu() {
+    console.log(
+        "Animating,  ",pos
+    )
+    if (menuOpen == true && pos <= 5) {
+        pos += 7;
+        document.getElementById("menu").style = "margin-top: "+pos+"px";
+      }
+    if (menuOpen == false && pos >= -320) {
+        pos -= 7;
+        document.getElementById("menu").style = "margin-top: "+pos+"px";
+      }
+}
+
+setInterval("animatemenu();", 2);
